ea04f1399b84b79457631a3bdf198976033df2d7
Catherine Jiang <catejiang@wustl.edu>
2017-12-02 10:24:36 -0600

    working on like/pass buttons

---
+++ petTinder/petTinder/petlist/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include,url
+from django.conf.urls.static import static
+
+from . import views
+
+urlpatterns = [
+    url(r'^userpets/$',views.userpets,name='userpets'),
+    url(r'^userpets/new/$',views.create_pet,name='create_pet'),
+]
