8693b8d1b96e8edcd6e3e7c304b818d54de1aff6
Catherine Jiang <catejiang@wustl.edu>
2017-12-02 19:34:39 -0600

    committing to fix format

--- petTinder/petTinder/editprofile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include,url
-from django.conf.urls.static import static
-
-from . import views
-
-urlpatterns = [
-    url(r'^profile/$', views.profile, name='profile'),
-    url(r'^del_user/$',views.del_user,name='del_user'),
-]
--- petTinder/petTinder/follow/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include,url
-from django.conf.urls.static import static
-
-from . import views
-
-urlpatterns = [
-    url(r'^allusers/$',views.allusers,name='allusers'),
-]
